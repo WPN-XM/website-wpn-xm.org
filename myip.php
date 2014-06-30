3edd883f1152959ae05ae7abd667533bcbba6753
Jens-André Koch <jakoch@web.de>
2014-06-30 21:02:55 +0200

    === operator

--- myip.php
+++ myip.php
@@ -1,7 +1,7 @@
 <?php
-    $ip = $_SERVER['HTTP_X_REAL_IP']; # $_SERVER['REMOTE_ADDR'];
-    if (preg_match('/^\d+\.\d+\.\d+\.\d+$/', $ip) == 1) {
-        echo $ip;
-    } else {
-        echo '0.0.0.0';
-    }
+$ip = $_SERVER['HTTP_X_REAL_IP']; # $_SERVER['REMOTE_ADDR'];
+if (preg_match('/^\d+\.\d+\.\d+\.\d+$/', $ip) === 1) {
+    echo $ip;
+} else {
+    echo '0.0.0.0';
+}
