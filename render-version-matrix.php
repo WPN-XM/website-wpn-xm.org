e0344657ea0786f58f8b5c1d4dcc59090a828ad5
jakoch <jakoch@web.de>
2014-08-11 14:43:20 +0200

    removed wpnxm prefix

--- render-version-matrix.php
+++ render-version-matrix.php
@@ -42,60 +42,60 @@ if(empty($wizardFiles) === true) {
 }
 $wizardRegistries = array();
 foreach($wizardFiles as $file) {
-	$name = str_replace('wpnxm-software-registry-', '', basename($file, '.json'));
-	$wizardRegistries[$name] = fixArraySoftwareAsKey(json_decode(file_get_contents($file), true));
+    $name = basename($file, '.json');
+    $wizardRegistries[$name] = fixArraySoftwareAsKey(json_decode(file_get_contents($file), true));
 }
 
 function fixArraySoftwareAsKey($array) {
-	$out = array();
-	foreach($array as $key => $values) {
-		$software = $values[0];
-		unset($values[0]);
-		$out[$software] = $values[3];
-	}
-	return $out;
+    $out = array();
+    foreach($array as $key => $values) {
+        $software = $values[0];
+        unset($values[0]);
+        $out[$software] = $values[3];
+    }
+    return $out;
 }
 
 function getVersion($registry, $software)
 {
-	if(isset($registry[$software]) === true) {
-		return '<span class="badge badge-info">' . $registry[$software] . '</span>';
-	}
-	return '&nbsp;';
+    if(isset($registry[$software]) === true) {
+        return '<span class="badge badge-info">' . $registry[$software] . '</span>';
+    }
+    return '&nbsp;';
 }
 
 function renderCell($registry, $software)
 {
-	return '<td>' . isVersion($registry, $software) . '</td>';
+    return '<td>' . isVersion($registry, $software) . '</td>';
 }
 
 function renderTableHeader($wizardRegistries)
 {
-	$header = '';
-	foreach($wizardRegistries as $wizardName => $wizardRegistry) {
-		$header .= '<td>' . $wizardName. '</td>';
-	}
-	return $header;
+    $header = '';
+    foreach($wizardRegistries as $wizardName => $wizardRegistry) {
+        $header .= '<td>' . $wizardName. '</td>';
+    }
+    return $header;
 }
 
 function renderTableCells($wizardRegistries, $software)
 {
-	$cells = '';
-	foreach($wizardRegistries as $wizardName => $wizardRegistry) {
-		$cells .= '<td>' . getVersion($wizardRegistry, $software) . '</td>';
-	}
-	return $cells;
+    $cells = '';
+    foreach($wizardRegistries as $wizardName => $wizardRegistry) {
+        $cells .= '<td>' . getVersion($wizardRegistry, $software) . '</td>';
+    }
+    return $cells;
 }
 ?>
 
 <table class="table table-condensed table-bordered">
 <thead>
-	<th>Software</th> <?php echo renderTableHeader($wizardRegistries); ?>
+    <th>Software</th> <?php echo renderTableHeader($wizardRegistries); ?>
 </thead>
 <?php
 foreach($registry as $software => $data)
 {
-	echo '<tr><td>' . $software . '</td>' . renderTableCells($wizardRegistries, $software) . '</tr>';
+    echo '<tr><td>' . $software . '</td>' . renderTableCells($wizardRegistries, $software) . '</tr>';
 }
 ?>
 </table>
