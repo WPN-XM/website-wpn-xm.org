40ace126514c18a868aea8b966a8f6d6e7ecb9e5
jakoch <jakoch@web.de>
2014-08-26 16:37:28 +0200

    switching from GPLv2+ to MIT license

--- myip.php
+++ myip.php
@@ -1,5 +1,15 @@
 <?php
-$ip = $_SERVER['HTTP_X_REAL_IP']; # $_SERVER['REMOTE_ADDR'];
+/**
+ * WPИ-XM Server Stack
+ * Copyright © 2010 - 2014 Jens-André Koch <jakoch@web.de>
+ * http://wpn-xm.org/
+ *
+ * This source file is subject to the terms of the MIT license.
+ * For full copyright and license information, view the bundled LICENSE file.
+ */
+
+$ip = $_SERVER['HTTP_X_REAL_IP'];
+
 if (preg_match('/^\d+\.\d+\.\d+\.\d+$/', $ip) === 1) {
     echo $ip;
 } else {
