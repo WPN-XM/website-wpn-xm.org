8d45aaaae23850af02d86e48c29aa97848f1c1ed
jakoch <jakoch@web.de>
2015-03-09 23:05:26 +0100

    return correct PHP version for "major.minor" requests (p=5.4, p=5.6)

--- get.php
+++ get.php
@@ -54,6 +54,24 @@ if (!empty($s) && array_key_exists($s, $registry)) {
             // software does not exist, download will fail.
             header("HTTP/1.0 404 Not Found");
         }
+    } elseif($s === 'php' or $s === 'php-x64') {
+        // it's either a specific version "v" or
+        // the latest patch version of a "major.minor" version "p"
+
+        // does the requested version exist?
+        if (!empty($v) && array_key_exists($v, $registry[$s])) {
+            // yes, return download url
+            header("Location: " . $registry[$s][$v]); // e.g. $registry['php']['1.2.1'];
+        } else {
+            // $_GET['p'] = php version, default version is php 5.5
+            $p = ($p = filter_input(INPUT_GET, 'p', FILTER_SANITIZE_STRING)) ? $p : '5.5';
+
+            // we have to get the latest "patch version" of a given "major.minor" version.
+            $minVersionConstraint = $p; // 5.4, 5.5, 5.6
+            $maxVersionConstraint = $minVersionConstraint . '.99'; // 5.4.99, 5.5.99
+            $version = getLatestVersion($s, $minVersionConstraint, $maxVersionConstraint);
+            header("Location: " . $registry[$s][$version]);
+        }
     } else {
         // Normal version => url relationships
 
@@ -69,4 +87,41 @@ if (!empty($s) && array_key_exists($s, $registry)) {
 } else {
     // software does not exist, download will fail.
     header("HTTP/1.0 404 Not Found");
+}
+
+/**
+ * Returns the latest version of a component given a min max version constraint.
+ * For example, latest version for the component PHP means "latest version for PHP5.4, PHP5.5, PHP5.6".
+ * It's a request for the "latest patch version" of a given "major.minor" version.
+ */
+function getLatestVersion($component, $minConstraint = null, $maxConstraint = null)
+{
+    global $registry;
+
+    if ($minConstraint === null && $maxConstraint === null) {
+        return $registry[$component]['latest']['version'];
+    }
+
+    // determine latest version for a component given a min/max constraint
+    $software = $registry[$component];
+    // remove all non-version stuff
+    unset($software['name'], $software['latest'], $software['website']);
+    // the array is already sorted.
+    // get rid of (version => url) and use (idx => version)
+    $software = array_keys($software);
+    // reverse array, in order to have the highest version number on top.
+    $software = array_reverse($software);
+    // reduce array to values in constraint range
+    foreach ($software as $url => $version) {
+      if (version_compare($version, $minConstraint, '>=') === true && version_compare($version, $maxConstraint, '<') === true) {
+          #echo 'Version v' . $version . ' is greater v' . $minConstraint . '(MinConstraint) and smaller v' . $maxConstraint . '(MaxConstraint).<br>';
+      } else {
+          unset($software[$url]);
+      }
+    }
+
+    // pop off the first element
+    $latestVersion = array_shift($software);
+
+    return $latestVersion;
 }
