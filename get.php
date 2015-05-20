5661bab646162b750c59c4b2411dac596ae8270b
jakoch <jakoch@web.de>
2015-05-20 14:03:21 +0200

    rewrote the GET script and added tests to address the phpext_phalcon issue, https://github.com/WPN-XM/WPN-XM/issues/390

--- get.php
+++ get.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * WPИ-XM Server Stack
  * Copyright © 2010 - 2014 Jens-André Koch <jakoch@web.de>
@@ -14,114 +15,364 @@
  * The script provides a header response to a software and version request
  * in form of a header redirection to the download url.
  */
+class Registry implements ArrayAccess
+{
+    public $registry;
 
-// load software components registry
-$registry = include __DIR__ . '/registry/wpnxm-software-registry.php';
+    public function __construct()
+    {
+        $this->loadRegistry();
+    }
 
-// ensure registry array is available
-if (!is_array($registry)) {
-    header("HTTP/1.0 404 Not Found");
-}
+    public function getUrl($software, $version, $bitsize = null, $phpVersion = null)
+    {
+        return $this->registry[$software][$version][$bitsize][$phpVersion];
+    }
 
-// $_GET['s'] = software component
-$s = filter_input(INPUT_GET, 's', FILTER_SANITIZE_STRING);
-// $_GET['v'] = version
-$v = filter_input(INPUT_GET, 'v', FILTER_SANITIZE_STRING);
-$v = ($v === 'latest') ? null : $v; // unset any latest requests, it defaults to latest
+    public function loadRegistry()
+    {
+        // load software components registry
+        $this->registry = include __DIR__ . '/registry/wpnxm-software-registry.php';
 
-// does the requested software exist in our registry?
-if (!empty($s) && array_key_exists($s, $registry)) {
+        // ensure registry array is available
+        if (!is_array($this->registry)) {
+            header('HTTP/1.0 404 Not Found');
+        }
+
+        return $this->registry;
+    }
 
     /**
-     * If the software component is a PHP extension, then
-     * we have to take the php_version into account, when fetching the url.
-     * The version => url relationship has one level more: version => php_version => url.
+     * Does the requested software exists in our registry?
      */
-    if(strpos($s, 'phpext_') !== false) {
-        // $_GET['p'] = php version for extensions, default version is php 5.5
-        $p = ($p = filter_input(INPUT_GET, 'p', FILTER_SANITIZE_STRING)) ? $p : '5.5';
-        // $_GET['bitsize'] = php bitsize for extensions, default version is x86
-        $bitsize = ($bitsize = filter_input(INPUT_GET, 'bitsize', FILTER_SANITIZE_STRING)) ? $bitsize : 'x86';
-
-        // does the requested version exist?
-        if (!empty($v) && array_key_exists($v, $registry[$s]) && array_key_exists($p, $registry[$s][$v][$bitsize])) {
-            // yes, return download url
-            header("Location: " . $registry[$s][$v][$bitsize][$p]); // e.g. $registry['phpext_xdebug']['1.2.1']['x86']['5.5'];
-        } elseif(array_key_exists($p, $registry[$s]['latest']['url'][$bitsize])) {
-            // no, requested version not existing, return latest version for php default version instead
-            header("Location: " . $registry[$s]['latest']['url'][$bitsize][$p]); // e.g. $registry['phpext_xdebug']['latest']['url']['x86']['5.5'];
-        } else {
-            // software does not exist, download will fail.
-            header("HTTP/1.0 404 Not Found");
-        }
-    } elseif($s === 'php' or $s === 'php-x64') {
-        // it's either a specific version "v" or
-        // the latest patch version of a "major.minor" version "p"
-
-        // does the requested version exist?
-        if (!empty($v) && array_key_exists($v, $registry[$s])) {
-            // yes, return download url
-            header("Location: " . $registry[$s][$v]); // e.g. $registry['php']['1.2.1'];
-        } else {
-            // $_GET['p'] = php version, default version is php 5.5
-            $p = ($p = filter_input(INPUT_GET, 'p', FILTER_SANITIZE_STRING)) ? $p : '5.5';
-
-            // we have to get the latest "patch version" of a given "major.minor" version.
-            $minVersionConstraint = $p; // 5.4, 5.5, 5.6
-            $maxVersionConstraint = $minVersionConstraint . '.99'; // 5.4.99, 5.5.99
-            $version = getLatestVersion($s, $minVersionConstraint, $maxVersionConstraint);
-            header("Location: " . $registry[$s][$version]);
-        }
-    } else {
-        // Normal version => url relationships
-
-        // does the requested version exist?
-        if (!empty($v) && array_key_exists($v, $registry[$s])) {
-            // yes, return download url
-            header("Location: " . $registry[$s][$v]); // e.g. $registry['nginx']['1.2.1'];
-        } else {
-            // no, requested version not existing, return latest version instead
-            header("Location: " . $registry[$s]['latest']['url']); // e.g. $registry['nginx']['latest']['url'];
-        }
+    public function softwareExists($software)
+    {
+        return (!empty($software) && array_key_exists($software, $this->registry));
     }
-} else {
-    // software does not exist, download will fail.
-    header("HTTP/1.0 404 Not Found");
+
+    /**
+     * Does the requested version of a software exist in our registry?
+     */
+    public function versionExists($software, $version)
+    {
+        return (!empty($version) && array_key_exists($version, $this->registry[$software]));
+    }
+
+    public function extensionHasPhpVersion($software, $version, $bitsize, $phpVersion)
+    {
+        return array_key_exists($phpVersion, $this->registry[$software][$version][$bitsize]);
+    }
+
+    public function extensionLatestVersionHasPHPVersion($software, $bitsize, $phpVersion)
+    {
+        return array_key_exists($phpVersion, $this->registry[$software]['latest']['url'][$bitsize]);
+    }
+
+    /**
+     * Is the software a PHP extension?
+     */
+    public function softwareIsPHPExtension($software)
+    {
+        return (strpos($software, 'phpext_') !== false);
+    }
+
+    /**
+     * Return the latest version of an PHP extensions
+     * given "name", "major.minor" "phpVersion" and "bitsize".
+     *
+     * $registry['phpext_xdebug']['1.2.1']['x86']['5.5.*']
+     */
+    public function getLatestVersion($software)
+    {
+        $versions = $this->getVersions($software);
+
+        end($versions);
+
+        $latest_version = key($versions);
+
+        return $latest_version;
+    }
+
+    /**
+     * Returns the version array for a component from the registry.
+     */
+    public function getVersions($component)
+    {
+        $component = $this->registry[$component];
+
+        // drop all non-version keys
+        unset($component['name'], $component['latest'], $component['website']);
+
+        return $component;
+    }
+
+    public function getPhpVersionInRange($software, $version, $bitsize, $phpVersion)
+    {
+        $array      = $this->registry[$software][$version][$bitsize];
+        $phpVersion = $this->getLatestVersionOfRange($array, $phpVersion . '.0', $phpVersion . '.99');
+
+        return $phpVersion;
+    }
+
+    /**
+     * Returns the latest version of a component inside a min/max version range.
+     *
+     * Example: fetch the "latest patch version" of a given "major.minor" version (5.4.*).
+     * getLatestVersion('php', '5.4.1', '5.4.99') = "5.4.30".
+     *
+     * @param array Only the versions array for this component from the registry.
+     * @param string A version number, setting the minimum (>=).
+     * @param string A version number, setting the maximum (<).
+     *
+     * @return string Returns the latest version of a component given a min max version constraint.
+     */
+    public function getLatestVersionOfRange($versions, $minConstraint = null, $maxConstraint = null)
+    {
+        // get rid of (version => url) and use (idx => version)
+        $versions = array_keys($versions);
+
+        // reverse array, in order to have the highest version number on top
+        $versions = array_reverse($versions);
+
+        // reduce array to values in constraint range
+        foreach ($versions as $idx => $version) {
+
+            // fix "5.y" to "5.y.1"
+            if (strlen($version) === 3) {
+                $version = $version . '.1';
+            }
+
+            if (version_compare($version, $minConstraint, '>=') === true && version_compare($version, $maxConstraint, '<') === true) {
+                #echo 'Version v' . $version . ' is greater v' . $minConstraint . '(MinConstraint) and smaller v' . $maxConstraint . '(MaxConstraint).<br>';
+            } else {
+                unset($versions[$idx]);
+            }
+        }
+
+        // pop off the first element
+        $latestVersion = array_shift($versions);
+
+        return $latestVersion;
+    }
+
+    /**
+     * ArrayAccess for Registry.
+     */
+    public function offsetSet($offset, $value)
+    {
+        return true;
+    }
+
+    public function offsetExists($offset)
+    {
+        return isset($this->registry[$offset]);
+    }
+
+    public function offsetUnset($offset)
+    {
+        unset($this->registry[$offset]);
+    }
+
+    public function offsetGet($offset)
+    {
+        return isset($this->registry[$offset]) ? $this->registry[$offset] : null;
+    }
 }
 
-/**
- * Returns the latest version of a component given a min max version constraint.
- * For example, latest version for the component PHP means "latest version for PHP5.4, PHP5.5, PHP5.6".
- * It's a request for the "latest patch version" of a given "major.minor" version.
- */
-function getLatestVersion($component, $minConstraint = null, $maxConstraint = null)
+class Request
 {
-    global $registry;
+    public $software;
+    public $version;
+    public $phpVersion;
+    public $bitsize;
 
-    if ($minConstraint === null && $maxConstraint === null) {
-        return $registry[$component]['latest']['version'];
+    public function __construct()
+    {
+        if (defined('PHPUNIT_TESTSUITE') === 1) {
+            $this->processGetDuringTesting();
+        } else {
+            $this->processGet();
+        }
     }
 
-    // determine latest version for a component given a min/max constraint
-    $software = $registry[$component];
-    // remove all non-version stuff
-    unset($software['name'], $software['latest'], $software['website']);
-    // the array is already sorted.
-    // get rid of (version => url) and use (idx => version)
-    $software = array_keys($software);
-    // reverse array, in order to have the highest version number on top.
-    $software = array_reverse($software);
-    // reduce array to values in constraint range
-    foreach ($software as $url => $version) {
-      if (version_compare($version, $minConstraint, '>=') === true && version_compare($version, $maxConstraint, '<') === true) {
-          #echo 'Version v' . $version . ' is greater v' . $minConstraint . '(MinConstraint) and smaller v' . $maxConstraint . '(MaxConstraint).<br>';
-      } else {
-          unset($software[$url]);
-      }
+    public function processGet()
+    {
+        // $_GET['s'] = software component
+        $this->software = filter_input(INPUT_GET, 's', FILTER_SANITIZE_STRING);
+
+        // $_GET['v'] = version
+        $version = filter_input(INPUT_GET, 'v', FILTER_SANITIZE_STRING);
+        // unset any latest version requests, because we return "latest version" by default
+        $this->version = ($version === 'latest') ? null : $version;
+
+        // $_GET['p'] = php version, default version is php 5.5
+        $this->phpVersion = ($phpVersion = filter_input(INPUT_GET, 'p', FILTER_SANITIZE_STRING)) ? $phpVersion : '5.5';
+
+        // $_GET['bitsize'] = php bitsize for extensions, default version is x86
+        $this->bitsize = ($bitsize = filter_input(INPUT_GET, 'bitsize', FILTER_SANITIZE_STRING)) ? $bitsize : 'x86';
     }
 
-    // pop off the first element
-    $latestVersion = array_shift($software);
+    /**
+     * Same as above, but using filter_var() in order to set and modify the $_GET superglobal during testing.
+     */
+    public function processGetDuringTesting()
+    {
+        $this->software = filter_var($_GET['s'], FILTER_SANITIZE_STRING);
 
-    return $latestVersion;
+        if (isset($_GET['v'])) {
+            $version       = filter_var($_GET['v'], FILTER_SANITIZE_STRING);
+            $this->version = ($version === 'latest') ? null : $version;
+        }
+
+        $this->phpVersion = '5.5';
+        if (isset($_GET['p'])) {
+            $this->phpVersion = ($phpVersion = filter_var($_GET['p'], FILTER_SANITIZE_STRING)) ? $phpVersion : '5.5';
+        }
+
+        $this->bitsize = 'x86';
+        if (isset($_GET['bitsize'])) {
+            $this->bitsize = ($bitsize = filter_var($_GET['bitsize'], FILTER_SANITIZE_STRING)) ? $bitsize : 'x86';
+        }
+    }
+}
+
+class Response
+{
+    public $url    = '';
+    public $header = '';
+
+    public function setUrl($url)
+    {
+        $this->url = $url;
+    }
+
+    public function setHeader($header)
+    {
+        $this->header = $header;
+    }
+
+    /**
+     * Redirect to the target url.
+     *
+     * @param string $url
+     */
+    public function redirect($url = null)
+    {
+        if (isset($url)) {
+            $this->url = $url;
+        }
+
+        $this->header = 'Location: ' . $this->url;
+
+        if (defined('PHPUNIT_TESTSUITE') === 1) {
+            #echo $this->header;
+            return;
+        } else {
+            header($this->header);
+            exit;
+        }
+    }
+
+    public function send()
+    {
+        if (defined('PHPUNIT_TESTSUITE') === 1) {
+            #echo $this->header;
+            return;
+        } else {
+            header($this->header);
+            exit;
+        }
+    }
+}
+
+### Script ####
+
+$request  = new Request();
+$response = new Response();
+$registry = new Registry();
+
+$handler = new Handler($request, $response, $registry);
+$handler->exec();
+
+class Handler
+{
+    public $registry;
+    public $request;
+    public $response;
+
+    public function __construct($request, $response, $registry)
+    {
+        $this->response = $response;
+        $this->request  = $request;
+        $this->registry = $registry;
+    }
+
+    public function exec()
+    {
+        if (!defined('PHPUNIT_TESTSUITE')) {
+            if (!$this->registry->softwareExists($this->request->software)) {
+                $this->response->setHeader('HTTP/1.0 404 Not Found');
+                $this->response->send();
+            }
+        }
+
+        /*
+         * If the software component is a PHP extension, then
+         * we have to take the "phpVersion" and "bitsize" into account, when fetching the url.
+         * The "version" to "url" relationship has two levels more: "version" to "bizsize" to phpVersion" to "url".
+         */
+        if ($this->registry->softwareIsPHPExtension($this->request->software)) {
+            if ($this->registry->versionExists($this->request->software, $this->request->version) &&
+                $this->registry->extensionsHasPhpVersion($this->request->software, $this->request->version, $this->request->bitsize, $this->request->phpVersion)) {
+
+                // return download url for specific version, e.g. $registry['phpext_xdebug']['1.2.1']['x86']['5.5']
+                $url = $this->registry[$this->request->software][$this->request->version][$this->request->bitsize][$this->request->phpVersion];
+
+                $this->response->redirect($url);
+            } elseif ($this->request->software === 'phpext_phalcon') {
+
+                // special handling for phpext_phalcon, because it has a PHP "patch level" version constraint.
+                // (while all other PHP extensions have only a "major.minor" version constraint.)
+                $version    = $this->registry->getLatestVersion($this->request->software);
+                $phpVersion = $this->registry->getPhpVersionInRange($this->request->software, $version, $this->request->bitsize, $this->request->phpVersion);
+                $url        = $this->registry[$this->request->software][$version][$this->request->bitsize][$phpVersion];
+
+                $this->response->redirect($url);
+            } elseif ($this->registry->extensionLatestVersionHasPhpVersion($this->request->software, $this->request->bitsize, $this->request->phpVersion)) {
+
+                // the specific version does not exist. return latest version for php default version instead,
+                // e.g. $registry['phpext_xdebug']['latest']['url']['x86']['5.5']
+                $this->response->redirect(
+                    $this->registry[$this->request->software]['latest']['url'][$this->request->bitsize][$this->request->phpVersion]
+                );
+            } else {
+                // software does not exist, download will fail.
+                $this->response->setHeader('HTTP/1.0 404 Not Found');
+                $this->response->send();
+            }
+        } else {
+
+            /*
+             * These are simple "version" to "url" relationships.
+             * There are no other constrains, like "phpVersion" or "bitsize" to consider.
+             */
+
+            if ($this->registry->versionExists($this->request->software, $this->request->version)) {
+                // return download url for specific version, e.g. $registry['nginx']['1.2.1']
+
+                $this->response->redirect($this->registry[$this->request->software][$this->request->version]);
+            } elseif ($this->request->software === 'php' or $this->request->software === 'php-x64') {
+                // special handling for PHP, because we have to
+                // return the latest patch version (x.y.*) of a "major.minor" PHP version (x.y)
+
+                $version = getLatestVersion($this->registry, $this->request->software, $this->request->version);
+                $this->response->redirect($this->registry[$this->request->software][$this->request->version]);
+            } else {
+                // return latest version url, e.g. $registry['nginx']['latest']['url']
+
+                $url = $this->registry[$this->request->software]['latest']['url'];
+                $this->response->redirect($url);
+            }
+        }
+    }
 }
---
+++ tests/GetTest.php
@@ -0,0 +1,81 @@
+<?php
+
+class GetTest extends PHPUnit_Framework_TestCase
+{
+    public function testRequestLatestVersion()
+    {
+        $url = 'http://wpn-xm.org/get.php?s=nginx';
+
+        $this->setGetRequest($url);
+
+        include dirname(__DIR__) . '/get.php';
+
+        $this->assertEquals(
+            'http://nginx.org/download/nginx-1.9.0.zip',
+            $handler->response->url
+        );
+    }
+
+    public function testRequestSpecificVersion()
+    {
+        $url = 'http://wpn-xm.org/get.php?s=nginx&v=1.0.0';
+
+        $this->setGetRequest($url);
+
+        include dirname(__DIR__) . '/get.php';
+
+        $this->assertEquals(
+            'http://www.nginx.org/download/nginx-1.0.0.zip',
+            $handler->response->url
+        );
+    }
+
+    public function testRequest_PHPExtension_Phalcon_WithShortPHPVersion54()
+    {
+        $url = 'http://wpn-xm.org/get.php?s=phpext_phalcon&p=5.4';
+
+        $this->setGetRequest($url);
+
+        include dirname(__DIR__) . '/get.php';
+
+        $this->assertEquals(
+            'http://static.phalconphp.com/files/phalcon_x86_VC9_php5.4.0_2.0.1_nts.zip',
+            $handler->response->url
+        );
+    }
+
+    public function testRequest_PHPExtension_Phalcon_WithShortPHPVersion55()
+    {
+        $url = 'http://wpn-xm.org/get.php?s=phpext_phalcon&p=5.5';
+
+        $this->setGetRequest($url);
+
+        include dirname(__DIR__) . '/get.php';
+
+        $this->assertEquals(
+            'http://static.phalconphp.com/files/phalcon_x86_VC11_php5.5.0_2.0.1_nts.zip',
+            $handler->response->url
+        );
+    }
+
+    public function testRequest_PHPExtension_Trader_LatestVersion54()
+    {
+        $url = 'http://wpn-xm.org/get.php?s=phpext_trader&p=5.4';
+
+        $this->setGetRequest($url);
+
+        include dirname(__DIR__) . '/get.php';
+
+        $this->assertEquals(
+            'http://windows.php.net/downloads/pecl/releases/trader/0.4.0/php_trader-0.4.0-5.4-nts-VC9-x86.zip',
+            $handler->response->url
+        );
+    }
+
+    public function setGetRequest($url)
+    {
+        $query = parse_url($url, PHP_URL_QUERY);
+        parse_str($query, $array);
+        $_GET = $array;
+    }
+}
