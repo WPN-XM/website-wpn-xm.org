b4583ba39d47a7dc3b23bf2fa59356d9510e2d0c
jakoch <jakoch@web.de>
2015-11-01 13:33:35 +0100

    cleanup

--- tests/GetTest.php
+++ tests/GetTest.php
@@ -10,15 +10,13 @@ class GetTest extends PHPUnit_Framework_TestCase
         $query = parse_url($url, PHP_URL_QUERY);
         parse_str($query, $array);
         $_GET = $array;
+                
+        include dirname(__DIR__) . '/get.php';
     }
 
     public function testRequestLatestVersion()
     {
-        $url = 'http://wpn-xm.org/get.php?s=nginx';
-
-        $this->setGetRequest($url);
-
-        include dirname(__DIR__) . '/get.php';
+        $this->setGetRequest('http://wpn-xm.org/get.php?s=nginx');
 
         $this->assertEquals(
             'http://nginx.org/download/nginx-1.9.0.zip',
@@ -28,11 +26,7 @@ class GetTest extends PHPUnit_Framework_TestCase
 
     public function testRequestSpecificVersion()
     {
-        $url = 'http://wpn-xm.org/get.php?s=nginx&v=1.0.0';
-
-        $this->setGetRequest($url);
-
-        include dirname(__DIR__) . '/get.php';
+        $this->setGetRequest('http://wpn-xm.org/get.php?s=nginx&v=1.0.0');
 
         $this->assertEquals(
             'http://www.nginx.org/download/nginx-1.0.0.zip',
@@ -42,11 +36,7 @@ class GetTest extends PHPUnit_Framework_TestCase
 
     public function testRequest_PHPExtension_Phalcon_WithShortPHPVersion54()
     {
-        $url = 'http://wpn-xm.org/get.php?s=phpext_phalcon&p=5.4';
-
-        $this->setGetRequest($url);
-
-        include dirname(__DIR__) . '/get.php';
+        $this->setGetRequest('http://wpn-xm.org/get.php?s=phpext_phalcon&p=5.4');
 
         $this->assertEquals(
             'http://static.phalconphp.com/files/phalcon_x86_VC9_php5.4.0_2.0.1_nts.zip',
@@ -56,11 +46,7 @@ class GetTest extends PHPUnit_Framework_TestCase
 
     public function testRequest_PHPExtension_Phalcon_WithShortPHPVersion540()
     {
-        $url = 'http://wpn-xm.org/get.php?s=phpext_phalcon&p=5.4.0';
-
-        $this->setGetRequest($url);
-
-        include dirname(__DIR__) . '/get.php';
+        $this->setGetRequest('http://wpn-xm.org/get.php?s=phpext_phalcon&p=5.4.0');
 
         $this->assertEquals(
             'http://static.phalconphp.com/files/phalcon_x86_VC9_php5.4.0_2.0.1_nts.zip',
@@ -70,11 +56,7 @@ class GetTest extends PHPUnit_Framework_TestCase
 
     public function testRequest_PHPExtension_Phalcon_WithShortPHPVersion55()
     {
-        $url = 'http://wpn-xm.org/get.php?s=phpext_phalcon&p=5.5';
-
-        $this->setGetRequest($url);
-
-        include dirname(__DIR__) . '/get.php';
+        $this->setGetRequest('http://wpn-xm.org/get.php?s=phpext_phalcon&p=5.5');
 
         $this->assertEquals(
             'http://static.phalconphp.com/files/phalcon_x86_VC11_php5.5.0_2.0.1_nts.zip',
@@ -84,11 +66,7 @@ class GetTest extends PHPUnit_Framework_TestCase
 
     public function testRequest_PHPExtension_Trader_LatestVersion54()
     {
-        $url = 'http://wpn-xm.org/get.php?s=phpext_trader&p=5.4';
-
-        $this->setGetRequest($url);
-
-        include dirname(__DIR__) . '/get.php';
+        $this->setGetRequest('http://wpn-xm.org/get.php?s=phpext_trader&p=5.4');
 
         $this->assertEquals(
             'http://windows.php.net/downloads/pecl/releases/trader/0.4.0/php_trader-0.4.0-5.4-nts-VC9-x86.zip',
@@ -98,11 +76,7 @@ class GetTest extends PHPUnit_Framework_TestCase
 
     public function testRequest_PHP_LatestVersion54()
     {
-        $url = 'http://wpn-xm.org/get.php?s=php&p=5.4';
-
-        $this->setGetRequest($url);
-
-        include dirname(__DIR__) . '/get.php';
+        $this->setGetRequest('http://wpn-xm.org/get.php?s=php&p=5.4');
 
         $this->assertContains("http://windows.php.net/downloads/releases/", $handler->response->url);
         $this->assertContains("5.4", $handler->response->url);
@@ -110,11 +84,7 @@ class GetTest extends PHPUnit_Framework_TestCase
 
     public function testRequest_PHP_LatestVersion56()
     {
-        $url = 'http://wpn-xm.org/get.php?s=php&p=5.6';
-
-        $this->setGetRequest($url);
-
-        include dirname(__DIR__) . '/get.php';
+        $this->setGetRequest('http://wpn-xm.org/get.php?s=php&p=5.6');
 
         $this->assertContains("http://windows.php.net/downloads/releases/", $handler->response->url);
         $this->assertContains("5.6", $handler->response->url);
