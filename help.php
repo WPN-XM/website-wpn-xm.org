889d12c540f342e11a64a0989c59eef5e3ec18c9
jakoch <jakoch@web.de>
2015-12-07 13:47:52 +0100

    cleanup

--- help.php
+++ help.php
@@ -12,11 +12,16 @@
 /**
  * Help
  *
- * The script provides a header response to a help page request
- * in form of a header redirection to the wiki url of that help page.
+ * The script provides a header response to a help page request,
+ * in form of a header redirection to the Wiki URL of that help page.
  *
- * URL for Installation Wizard Help Button:
+ * The request comes from our installation wizards.
+ * All installation wizards provide a Help Button.
+ *
+ * A URL for Installation Wizard Help Button is
  * http://wpn-xm.org/help.php?section=install-wizard&type=webinstaller&page=1&version=0.6.0&language=de
+ *
+ * The URL is build automatically using the procedure HelpButtonClick() in the installer script.
  */
 
 // fetch $_GET parameters
@@ -32,8 +37,10 @@ $language = filter_input(INPUT_GET, 'language', FILTER_SANITIZE_STRING);
 // $_GET['type'] = type of installation wizard (lite, bigpack, allinone, web)
 $type = filter_input(INPUT_GET, 'type', FILTER_SANITIZE_STRING);
 
-// map for wizard page integers to their full page names
-// see http://www.jrsoftware.org/ishelp/index.php?topic=wizardpages
+/**
+ * map for wizard page integers to their full page names
+ * see http://www.jrsoftware.org/ishelp/index.php?topic=wizardpages
+ */
 $wizardPages = [
     '1' => 'welcome',
     '2' => 'license-agreement',
@@ -51,8 +58,10 @@ $wizardPages = [
    '14' => 'setup-completed',
 ];
 
-// map for wizard types with correct case
-// see line with "#define InstallerType" in the iss files
+/**
+ * map for wizard types with correct case
+ * see line with "#define InstallerType" in the iss files
+ */
 $wizardTypes = [
     'webinstaller' => 'Webinstaller',
     'allinone'     => 'AllInOne',
@@ -66,7 +75,11 @@ $wizardTypes = [
 ];
 
 if (!empty($type) && !empty($version) && !empty($language) && !empty($page)) {
-    // build URL - https://github.com/WPN-XM/WPN-XM/wiki/Installation-Wizard-Webinstaller-v0.6.0-de#Welcome
+
+    /**
+     * Example for a target URL:
+     * https://github.com/WPN-XM/WPN-XM/wiki/Installation-Wizard-Webinstaller-v0.6.0-de#Welcome
+     */
 
     $helpURL = sprintf(
         'https://github.com/WPN-XM/WPN-XM/wiki/Installation-Wizard-%s-v%s-%s#%s',
