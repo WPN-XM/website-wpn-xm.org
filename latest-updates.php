fc707e6db5f94635d42cc8a83944a8f3e06201ff
jakoch <jakoch@web.de>
2015-12-10 23:01:51 +0100

    do not run a col-md-3

--- latest-updates.php
+++ latest-updates.php
@@ -67,14 +67,14 @@ class LatestUpdates
 
     public function renderPanel()
     {
-        $html = '<div class="col-md-3">' . "\n";
+        //$html = '<div class="col-md-3">' . "\n";
         $html .= '<div class="panel panel-default">' . "\n";
         $html .= '<div class="panel-heading"><strong>Lastest Updates</strong></div>' . "\n";
         $html .= '<div class="panel-body">' . "\n";
         $html .= $this->renderLogItems();
         $html .= '</div>' . "\n";
         $html .= '</div>' . "\n";
-        $html .= '</div>' . "\n";
+        //$html .= '</div>' . "\n";
         return $html;
     }
 
